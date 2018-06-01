b932afffa192a403248db0df2ef56b841bf34c2c
james7132 <james.siqi.liu@gmail.com>
2018-05-31 22:08:42 -0700

    Basic Vue.js Frontend

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,51 @@
+<template>
+<b-navbar toggleable="md" type="dark" variant="dark">
+  <b-container>
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+    <b-navbar-brand href="#">Hakurei Expo</b-navbar-brand>
+
+    <b-collapse is-nav id="nav_collapse">
+
+      <b-navbar-nav>
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
+        </b-nav-form>
+        <b-nav-item href="/circles">Circles</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+
+        <b-nav-item-dropdown right>
+          <!-- Using button-content slot -->
+          <template slot="button-content">
+            <em>+</em>
+          </template>
+          <b-dropdown-item href="#">New Work</b-dropdown-item>
+          <b-dropdown-item href="#">New Circle</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+          <!-- Using button-content slot -->
+          <template slot="button-content">
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Signout</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+
+    </b-collapse>
+  </b-container>
+</b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-container>
+    <b-table hover :items="items"></b-table>
+  </b-container>
+</template>
+
+<script>
+const items = [
+    { name: "Fantasy Crescendo", creator: "Hourai Teahouse", type: "Fangame" },
+    { name: "Danmaku!!", creator: "Mystery Parifait", type: "Fangame" },
+    { name: "NitorInc: Touhou Microgames", creator: "NitorInc", type: "Fangame" },
+    { name: "PC-69 Project", creator: "Pantsu Syndicate", type: "Artbook" },
+    { name: "Touhou Orchestral Suite", creator: "crescentia", type: "Music (Album)" },
+]
+
+export default {
+  data() {
+    return {
+      items: items
+    }
+  }
+}
+</script>
