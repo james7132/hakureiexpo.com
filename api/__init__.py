b090e53931bc3dcb46c2821ccd80a862cb818eb8
james7132 <james.siqi.liu@gmail.com>
2018-06-19 23:37:26 -0700

    Add Firebase Auth as a Auth provider

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,14 @@
 import endpoints
 
-api_collection = endpoints.api(name='hakurei-expo', version='v1')
+# Add Firebase Auth as an auth provider
+firebase_auth = endpoints.Issuer(
+    'https://securetoken.google.com/hakurei-expo',
+    'https://www.googleapis.com/service_accounts/v1/metadata/x509/securetoken@system.gserviceaccount.com')
+
+api_collection = endpoints.api(
+    name='hakurei-expo',
+    version='v1',
+    issuers={'firebase': firebase_auth})
 
 from .users import UsersApi
 from .posts import UserPostsApi, CirclePostsApi
